55ce6b9ead6d001bf8bc91b0d9acd3ac5b2dae43
user <daniil.stepanenko.96@gmail.com>
2020-12-06 23:53:05 +0200

    app: add comment

--- src/app/app.go
+++ src/app/app.go
@@ -2,13 +2,16 @@ package app
 
 import "context"
 
+// New create App object
 func New() (*App, error) {
 	return &App{}, nil
 }
 
+// App is a main object
 type App struct {
 }
 
+// Run start application
 func (a App) Run(ctx context.Context) error {
 	// TODO
 	return nil
